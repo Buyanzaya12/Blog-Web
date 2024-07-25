e7be3e95d1939e53a7bebb881940dd17043b2365
Buyanzaya12 <157042363+Buyanzaya12@users.noreply.github.com>
2024-07-25 19:04:45 +0800

    Add files via upload

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,17 +1,20 @@
 import Image from "next/image";
 export function Header() {
     return (
-        <div className="flex items-center">
+        <div className="flex items-center p-5 justify-between">
+            <div className="flex flex-wrap items-center">
             <Image src="blog-web-pics/Union.svg" width={36} height={36} />
             <p className="text-black text-base ">Meta<span class="font-bold">Blog</span></p>
-            <div className="grid grid-cols-3 text-base text-gray-900">
+            </div>
+            <Image src="blog-web-pics/menu-outline.svg" width={32} height={32} className="flex justify-end"/>
+            {/* <div className="grid grid-cols-3 text-base text-gray-900 w-0 opacity-0">
                 <p>Home</p>
                 <p>Blog</p>
                 <p>Contact</p>
-            </div>
-            <input>
+            </div> */}
+            {/* <input className="opacity-0 w-0">
                 
-            </input>
+            </input> */}
         </div>
     )
 }
--- index.js
+++ index.js
@@ -4,17 +4,53 @@ import { Trend } from "@/components/Trend/Trend";
 import { Content } from "@/components/Content/Content";
 import { Upperfooter } from "@/components/Upperfooter/Upperfooter";
 import { Footer } from "@/components/Footer/Footer";
+import { Inter } from "next/font/google";
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dev.to/api/articles?username=paul_freeman")
+         .then((response) => {
+            return response.json();
+            })
+      .then((data) => {
+        setArticles(data);
+      });
+}, []);
   return (
-  <main className="mx-auto max-w-[1920px] bg-white">
+      /* All blog post */
+    <div className="container">
+    <div className="grid web:grid-cols-3 md:grid-cols-2 gap-2">
+      
+     
+      {articles.map((item) => (
+        <div key={item.id} className="shadow-lg card bg-base-100">
+          
+          <div className="card-body">
+          <Image src={item.social_image} width={360} height={240} />
+          <Link href={item.url} target="_blank"> 
+           {item.title}
+           </Link>
+           </div>
+          </div>
+      ))}
+      </div>
+      <main className="mx-auto w-[390px] max-w-[1920px] bg-white">
     <Header />
+    <div className="w-[325px] mx-auto">
     <Slider />
     <Trend />
     <Content />
     <Upperfooter />
     <Footer />
+    </div>
   </main>
-  );
-}
+    // </div>
+  )
+};
